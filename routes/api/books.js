1a2ebe0cde02be26fe077dd253f087c9090e3a66
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-12-30 16:45:01 -0800

    add routes/api

---
+++ routes/api/books.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const books = require("../../models/books/books.json");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await books.getAll();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
+
+module.exports = router;
